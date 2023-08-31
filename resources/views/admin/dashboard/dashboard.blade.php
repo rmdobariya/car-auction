de0910a35d9adaca0124bed330e75a2c329f6d64
rmdobariya <roshan.crestcoder@gmail.com>
2023-08-31 15:01:53 +0530

    autodeploy dashboard

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -6,7 +6,7 @@
                 <div data-kt-swapper="true" data-kt-swapper-mode="prepend"
                      data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
                      class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
-                    <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Dashboard</h1>
+                    <h1 class="d-flex align-items-center text-dark fw-bolder fs-3 my-1">Dashboard1</h1>
                 </div>
                 <div class="d-flex align-items-center gap-2 gap-lg-3">
                 </div>
