c948486cd3d33a3c3d5b340f7746fee757a84379
rmdobariya <roshan.crestcoder@gmail.com>
2023-09-28 12:56:10 +0530

    email header and footer change done

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Zodha'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://car-auction.projectdemo.click/'),
 
     'asset_url' => env('ASSET_URL'),
 
---
+++ resources/views/vendor/mail/text/header.blade.php
@@ -0,0 +1,1 @@
+[{{ 'EQUS ONLINE' }}]({{ $url }})
