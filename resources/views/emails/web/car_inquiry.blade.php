4194e441566005aed4bcb79b8d119c5de912131b
rmdobariya <roshan.crestcoder@gmail.com>
2023-09-28 14:11:00 +0530

    email header and footer and profile change done 2

--- resources/views/emails/web/car_inquiry.blade.php
+++ resources/views/emails/web/car_inquiry.blade.php
@@ -4,6 +4,7 @@
     First Name : {{$details['first_name']}}
     Last Name : {{$details['last_name']}}
     Email : {{$details['email']}}
+    Message : {{$details['request_msg']}}
     Thanks
     Zodha
 @endcomponent
